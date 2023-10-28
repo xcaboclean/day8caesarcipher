9cb876b78353727337e47e16bd4f64b933e93e32
xcaboclean <146889041+xcaboclean@users.noreply.github.com>
2023-10-28 21:50:37 +0000

    Initial commit

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
+lenght_alphabet = len(alphabet)
+print(f"lenght_alphabet: {lenght_alphabet}")
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
+
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-def encrypt(text, shift):
-    encrypted_text = ""
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
+
+#TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
     #e.g. 
     #plain_text = "hello"
     #shift = 5
@@ -20,3 +21,30 @@ def encrypt(text, shift):
     ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
 
 #TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
+def encrypt(text, shift):
+cipher_text = ""
+for position in range(len(text)):
+  letter = text[position]
+  new_position = alphabet.index(letter) + shift 
+
+  if new_position >= lenght_alphabet:
+    new_position = new_position % lenght_alphabet   
+
+  code_cipher = alphabet[new_position]
+  cipher_text += code_cipher
+print(cipher_text)
+
+def dencrypt(cipher_text, shift):
+text = ""
+for position in range(len(cipher_text)):
+  letter = cipher_text[position]
+  new_position = alphabet.index(letter) + shift 
+  if new_position < 0:
+    new_position = lenght_alphabet + new_position
+
+  code = alphabet[new_position]
+  text += code
+print(text)
+encrpyted_text = encrypt(text, shift)
+print("The encoded text is:", encrypted)
+dencrypted_text = dencrypt()
