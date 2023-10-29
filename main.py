06b80d8c2ee100a896a865f3f1d47ae9ba06661d
xcaboclean <146889041+xcaboclean@users.noreply.github.com>
2023-10-29 17:36:35 +0000

    Adjusting variable names

--- main.py
+++ main.py
@@ -1,27 +1,36 @@
-def encrypt(text, shift):
+def encrypt(plain_text, shift_amount):
+  
   cipher_text = ""
-  for position in range(len(text)):
-    letter = text[position]
-    new_position = alphabet.index(letter) + shift
+  
+  for letter in plain_text:
+    position  = alphabet.index(letter)
+    new_position = position + shift_amount
+    
     if new_position >= lenght_alphabet:
       new_position = new_position % lenght_alphabet
+      
     code_cipher = alphabet[new_position]
     cipher_text += code_cipher
-  print(cipher_text)
+    
+  print("The encoded text is:", encrypted_text)
 
-def dencrypt(cipher_text, shift):
-  text = ""
-  for position in range(len(cipher_text)):
-    letter = cipher_text[position]
-    new_position = alphabet.index(letter) - shift 
-    if new_position < 0:
-      new_position = lenght_alphabet + new_position
+def dencrypt(cipher_text, shift_amount):
+  plain_text = ""
+  for letter in cipher_text:
+    position = alphabet.index(letter)
+    new_position = position - shift_amount
+    
+    if position < 0:
+      new_position = lenght_alphabet + position
     code = alphabet[new_position]
-    text += code
-  print(text)
+    plain_text += code
+  print("The decode text is:", dencrypted_text)
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','-',' ']
+
+
 lenght_alphabet = len(alphabet)
+
 print(f"lenght_alphabet: {lenght_alphabet}")
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
@@ -29,7 +38,7 @@ shift = int(input("Type the shift number:\n"))
 
 if direction == "encode":
   encrypted_text = encrypt(text, shift)
-  print("The encoded text is:", encrypted_text)
-else:
+elif direction =="decode":
   dencrypted_text = dencrypt(text, shift)
-  print("The decode text is:", dencrypted_text)
+else:
+  print("Invalid direction.")
