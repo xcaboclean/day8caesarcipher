1ce0ea8dfdacd3a6c840a19c64e055ae16bf36c7
xcaboclean <146889041+xcaboclean@users.noreply.github.com>
2023-10-30 15:21:02 +0000

    print logo caesar cipher

--- main.py
+++ main.py
@@ -14,8 +14,10 @@ def caesar(start_text, shift_amount,cipher_direction):
     code= alphabet[new_position]
     final_text += code
   
-  print(f"The {cipher_direction} text is:", final_text)
+  print(f"The {cipher_direction} text is:", fi'nal_text)
 
+from art import logo
+print(logo)
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','-',' ']
 
