72d996762f21e0de5d8f75cfeb065746dcedaa6d
xcaboclean <146889041+xcaboclean@users.noreply.github.com>
2023-10-30 16:20:26 +0000

    repeat function

--- main.py
+++ main.py
@@ -4,29 +4,32 @@ def caesar(start_text, shift_amount,cipher_direction):
     shift_amount*= -1
     
   for letter in start_text:
-    position = alphabet.index(letter)
-     
-    new_position = position + shift_amount
-    
-    if new_position >= lenght_alphabet:
-       new_position = new_position % lenght_alphabet
-    
-    code= alphabet[new_position]
-    final_text += code
-  
-  print(f"The {cipher_direction} text is:", fi'nal_text)
+    if letter in alphabet:
+      position = alphabet.index(letter)  
+      new_position = position + shift_amount
+      if new_position >= lenght_alphabet:
+        new_position = new_position % lenght_alphabet
+      code= alphabet[new_position]
+      final_text += code
+    else:
+      final_text += letter
+      
+  print(f"The {cipher_direction} text is:", final_text)
 
 from art import logo
 print(logo)
-
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','-',' ']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
 lenght_alphabet = len(alphabet)
-
-print(f"lenght_alphabet: {lenght_alphabet}")
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-caesar(text, shift, direction)
+again = False
+while not again:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  shift = shift % lenght_alphabet
+  caesar(text, shift, direction)
+  restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+  if restart == "no":
+    again = True
+    print("Goodbye")
