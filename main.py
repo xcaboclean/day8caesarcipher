63723235444fd7d6ef2a01157c574ca97a5d21ff
xcaboclean <146889041+xcaboclean@users.noreply.github.com>
2023-10-30 15:13:57 +0000

    fix direction - out loop

--- main.py
+++ main.py
@@ -1,30 +1,21 @@
-def encrypt(plain_text, shift_amount):
-  
-  cipher_text = ""
-  
-  for letter in plain_text:
-    position  = alphabet.index(letter)
+def caesar(start_text, shift_amount,cipher_direction):
+  final_text = ""
+  if cipher_direction == "decode":
+    shift_amount*= -1
+    
+  for letter in start_text:
+    position = alphabet.index(letter)
+     
     new_position = position + shift_amount
     
     if new_position >= lenght_alphabet:
-      new_position = new_position % lenght_alphabet
-      
-    code_cipher = alphabet[new_position]
-    cipher_text += code_cipher
+       new_position = new_position % lenght_alphabet
     
-  print("The encoded text is:", encrypted_text)
+    code= alphabet[new_position]
+    final_text += code
+  
+  print(f"The {cipher_direction} text is:", final_text)
 
-def dencrypt(cipher_text, shift_amount):
-  plain_text = ""
-  for letter in cipher_text:
-    position = alphabet.index(letter)
-    new_position = position - shift_amount
-    
-    if position < 0:
-      new_position = lenght_alphabet + position
-    code = alphabet[new_position]
-    plain_text += code
-  print("The decode text is:", dencrypted_text)
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','-',' ']
 
@@ -36,9 +27,4 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-if direction == "encode":
-  encrypted_text = encrypt(text, shift)
-elif direction =="decode":
-  dencrypted_text = dencrypt(text, shift)
-else:
-  print("Invalid direction.")
+caesar(text, shift, direction)
